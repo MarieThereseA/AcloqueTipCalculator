179f364fec0e0af6960e955a38e39b74175e8c6b
1E10 Laptop <sample@bths.edu>
2022-09-22 12:32:37 -0400

    tip calc revised

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -12,17 +12,17 @@ public class TipCalc {
         double totalCost = 0;
         double tipPercent;
         double totalTip;
-        String totalTipRounded;
         double totalTipCost; // Total cost including tip
+        String totalTipCostRound;
         double perCost;
         double perTipCost; //Total cost per person including tip
         double perTip;
 
         //Greeting + # of people + tip percentage
-        System.println("Welcome to the Tip Calculator!");
-        System.print("How many people are in your group? ");
+        System.out.println("Welcome to the Tip Calculator!");
+        System.out.print("How many people are in your group? ");
         people = scan.nextInt();
-        System.print("What's the tip percentage? ");
+        System.out.print("What's the tip percentage? ");
         tipPercent = scan.nextDouble();
 
         //Total cost
@@ -36,7 +36,8 @@ public class TipCalc {
         //Calculations
         tipPercent /= 100;
         totalTip = totalCost * tipPercent;
-        totalTipRounded = formatter.format(totalTip);
-//
+        totalTipCost = totalCost + totalTip;
+
+
     }
 }
